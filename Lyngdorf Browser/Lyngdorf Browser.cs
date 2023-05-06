7dcca2c32fa876c4bf0f9bf6ce1917455ef5180c
Michael Morten Sonne <michael@sonnes.cloud>
2023-05-06 14:35:59 +0200

    Move cach and setup settings to disable debug log

--- Lyngdorf Browser/Lyngdorf Browser.cs
+++ Lyngdorf Browser/Lyngdorf Browser.cs
@@ -62,32 +62,6 @@ namespace LyngdorfBrowser
             {
                 MessageBox.Show(@"Failed to get the IP address for the Lyngdorf device found on your network or the device is not supported for this");
             }
-
-            // FUll MAC address
-            /*try
-            {
-                string ipAddress = IPMacMapper.FindIPFromMacAddress("50-1e-2d-1e-15-2a");
-                _url = ipAddress;
-            }
-            catch
-            {
-                MessageBox.Show(@"WiFI ik fundet");
-            }
-            try
-            {
-                string ipAddress = IPMacMapper.FindIPFromMacAddress("50-1e-2d-1e-15-28");
-                _url = ipAddress;
-            }
-            catch
-            {
-                MessageBox.Show(@"LAN ik fundet");
-            }
-
-            CefSettings settings = new CefSettings();
-            Cef.Initialize(settings);
-            ChromeBrowser = new ChromiumWebBrowser("http://" + _url);
-            Controls.Add(ChromeBrowser);
-            ChromeBrowser.Dock = DockStyle.Fill;*/
         }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
--- Lyngdorf Browser/Program.cs
+++ Lyngdorf Browser/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CefSharp.WinForms;
+using CefSharp;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace LyngdorfBrowser
@@ -14,6 +14,16 @@ namespace LyngdorfBrowser
         [STAThread]
         static void Main()
         {
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), Application.ProductName + "\\Cache")
+            };
+
+            settings.LogSeverity = LogSeverity.Disable;
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
