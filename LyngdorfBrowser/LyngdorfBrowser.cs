76765f8533145d8db902a0a65dec66ee7216bff5
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2023-06-17 13:21:07 +0200

    Added CheckConnection function

--- LyngdorfBrowser/LyngdorfBrowser.cs
+++ LyngdorfBrowser/LyngdorfBrowser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Timers;
 using System.Windows.Forms;
 using CefSharp;
 using CefSharp.WinForms;
@@ -12,10 +13,97 @@ namespace LyngdorfBrowser
         private string _url = string.Empty;
         private ChromiumWebBrowser _chromeBrowser;
         public string OriginalText;
+        public string TitleText;
+        public string StatusText;
+        private System.Timers.Timer _connectionTimer;
+        private bool _isTimerPaused;
 
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void ConnectionTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            CheckConnection();
+        }
+
+        private void CheckConnection()
+        {
+            try
+            {
+                if (_chromeBrowser != null && _chromeBrowser.IsBrowserInitialized)
+                {
+                    // Pause the timer
+                    _connectionTimer.Stop();
+
+                    // Execute JavaScript code to check if the website is still reachable
+                    _chromeBrowser.EvaluateScriptAsync("navigator.onLine").ContinueWith(task =>
+                    {
+                        if (task.IsCompleted && !task.IsFaulted)
+                        {
+                            var response = task.Result;
+                            if (response.Success && response.Result is bool isOnline && isOnline)
+                            {
+                                // Website is reachable
+                                StatusText = "Website is reachable";
+#if DEBUG
+                                _isTimerPaused = true;
+                                MessageBox.Show(@"Website '" + TitleText + @"' is reachable", @"Can connect", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                _isTimerPaused = false;
+#endif
+                            }
+                            else
+                            {
+                                // Website is not reachable
+                                if (!_isTimerPaused)
+                                {
+                                    _isTimerPaused = true;
+                                    MessageBox.Show(@"Website '" + TitleText + @"' is not reachable", @"Can't connect right now", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                    _isTimerPaused = false;
+                                }
+                                StatusText = "Website " + TitleText + @" is not reachable";
+                            }
+                        }
+                        else
+                        {
+                            // Error occurred while checking the connection
+                            if (!_isTimerPaused)
+                            {
+                                _isTimerPaused = true;
+                                MessageBox.Show(@"An error occurred while checking the connection to '" + TitleText + @"'.", @"Can't connect right now", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                _isTimerPaused = false;
+                            }
+                            StatusText = "An error occurred while checking the connection to '" + TitleText + @"'";
+                        }
+
+                        // Resume the timer
+                        _connectionTimer.Start();
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                if (!_isTimerPaused)
+                {
+                    _isTimerPaused = true;
+                    MessageBox.Show(@"An error occurred while checking the connection to: '" + TitleText + @"'." + Environment.NewLine + @"Error: " + ex.Message, @"Can't connect right now", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    _isTimerPaused = false;
+                }
+#endif
+                StatusText = "An error occurred while checking the connection to '" + TitleText + @"': " + ex.Message;
+
+                // Resume the timer
+                _connectionTimer.Start();
+            }
+
+            // Simulate the title change event
+            //Text = $@"{OriginalText}: {TitleText} - Status: " + StatusText;
+
+            // string newTitle = $@"{OriginalText}: {TitleText} - Status: " + StatusText;
+            // TitleChangedEventArgs args = new TitleChangedEventArgs(_chromeBrowser.GetBrowser(), newTitle);
+            // Browser_TitleChanged(_chromeBrowser, args);
         }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -46,23 +134,11 @@ namespace LyngdorfBrowser
                 // If no arguments is set, find device on the network from MAC address (vendor)
                 InitializeChromium();
             }
-        }
 
-        private void Browser_TitleChanged(object sender, TitleChangedEventArgs e)
-        {
-            Invoke(new Action(() =>
-            {
-                switch (Text.StartsWith(OriginalText))
-                {
-                    // Check if the original text has already been appended
-                    case false:
-                        Text = $@"{OriginalText}: {e.Title}";
-                        break;
-                    default:
-                        Text = $@"{OriginalText}: {e.Title}";
-                        break;
-                }
-            }));
+            // Start the connection checking timer
+            _connectionTimer = new System.Timers.Timer(5000); // Set the interval to 5 seconds (adjust as needed)
+            _connectionTimer.Elapsed += ConnectionTimerElapsed;
+            _connectionTimer.Start();
         }
 
         public void InitializeChromium(string ipAddress = null)
@@ -99,10 +175,34 @@ If this not works, use the commandline to connect to a specific IP address in th
             }
         }
 
+        private void Browser_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                switch (Text.StartsWith(OriginalText))
+                {
+                    // Check if the original text has already been appended
+                    case false:
+                        //TODO
+                        //Text = $@"{OriginalText}: {e.Title} - Status: " + StatusText;
+                        Text = $@"{OriginalText}: {e.Title}";
+                        break;
+                    default:
+                        Text = $@"{OriginalText}: {e.Title}";
+                        break;
+                }
+            }));
+
+            TitleText = e.Title;
+        }
+
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             try
             {
+                // Stop the connection checking timer
+                _connectionTimer?.Stop();
+
                 Cef.Shutdown();
             }
             catch (Exception exception)
